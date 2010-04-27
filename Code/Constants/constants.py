f2470c93e9298218f9c04e54c618c33d71264f60
ClockworkDemon <ClockworkDemon@de903a64-1c27-11df-96c8-59bb23a8caa6>
2010-04-27 02:48:43 +0000

    Comitted constants

---
+++ Code/Constants/constants.py
@@ -0,0 +1,30 @@
+#               LOL JK PUNK'D
+#       Have fun finding magic numbers! 
+#
+#                   _____
+#                  ||   ||
+#                  |\___/|
+#                  |     |
+#                  |     |
+#                  |     |
+#                  |     |
+#                  |     |
+#                  |<-@->|
+#              ____|     |____
+#         ___ /    |     |    \
+#        /    |    |     |    | \
+#        |    |    |     |    |  |
+#        |    |    |     |    |  |
+#        |                    |  |
+#        |                       /
+#        |                      /
+#         \                   /
+#          \                 /
+#           |                |
+#           |                |
+#
+# Wars Fought                   Stud Service
+# Revolutions Started           Tigers Tamed
+# Assassinations Plotted        Bars Emptied
+# Governments Run         Computers Verified
+# Uprisings Quelled         Orgies Organized
