3eaafe257667e33ff0076b264a13b68e77e65bc3
ClockworkDemon <ClockworkDemon@de903a64-1c27-11df-96c8-59bb23a8caa6>
2010-04-28 03:17:15 +0000

    Changed exampleAlgs

--- Code/Algorithm/exampleAlg.py
+++ Code/Algorithm/exampleAlg.py
@@ -1,13 +1,18 @@
 class Algorithm():
-    def __init__(self, USConstants):
-        self.USConstants = USConstants
+    def __init__(self):
+        self.constants = UserSetConstantContainer()
+        self.constants.add(UserSetConstant("a"))
+        self.constants.add(UserSetConstant("b"))
 
     def Run():
         return self.USConstants["a"]*self.USConstants["b"]*IMP.GetAlgorithms["otherAlg"]
 
 class Algorithm():
-    def __init__(self, USConstants):
-        self.USConstants = USConstants
+    def __init__(self):
+        self.constants = UserSetConstantContainer()
+        self.constants.add(UserSetConstant("c"))
+        self.constants.add(UserSetConstant("d"))
+
 
     def Run():
         return self.USConstants["c"]/self.USConstants["d"]
