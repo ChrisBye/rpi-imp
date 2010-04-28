2f339ddee62fee650f7cd8357a42f0f400ed6e58
ClockworkDemon <ClockworkDemon@de903a64-1c27-11df-96c8-59bb23a8caa6>
2010-04-28 03:11:53 +0000

    Example

---
+++ Code/Algorithm/exampleAlg.py
@@ -0,0 +1,13 @@
+class Algorithm():
+    def __init__(self, USConstants):
+        self.USConstants = USConstants
+
+    def Run():
+        return self.USConstants["a"]*self.USConstants["b"]*IMP.GetAlgorithms["otherAlg"]
+
+class Algorithm():
+    def __init__(self, USConstants):
+        self.USConstants = USConstants
+
+    def Run():
+        return self.USConstants["c"]/self.USConstants["d"]
