01413699a2141a95dfdb27444dba3e868764babd
ClockworkDemon <ClockworkDemon@de903a64-1c27-11df-96c8-59bb23a8caa6>
2010-04-30 00:29:42 +0000

    Made some modifications to MovingAverage, removed magic buttons, added proper backend to the Algorithm Window

--- trunk/Code/IMP.py
+++ trunk/Code/IMP.py
@@ -15,14 +15,6 @@ class IMP:
         # load algorithms
         # load scraper
 
-        self.algorithms = dict()
-        for inalg in glob.glob(os.path.join('Algorithms', "*.alg")):
-            #print "Current file is " + inalg
-            name = inalg.split(".alg")
-            name[0] = name[0].split("/")
-            #print inalg[0][1]
-            execfile(inalg)
-
         self.algwindow = AlgWindow(self)
         self.graphwindow = GraphWindow(self)
 
