dafeb6539b2b95929a77f87b80030b7a81c4c1b2
ClockworkDemon <ClockworkDemon@de903a64-1c27-11df-96c8-59bb23a8caa6>
2010-05-07 03:14:19 +0000

    Just fixin' stuff

--- Code/IMP.py
+++ Code/IMP.py
@@ -36,9 +36,8 @@ class IMP:
     def getStockPrice(self, symbol):
         return self.stockdata.stocks[symbol].price
 
-    def GetAlgorithms(self, algname):
-        # Currently bugged, see past revision
-        return 0
+    def GetAlgorithms(self, algname, symbol):
+        return self.algbackend.getAlgorithmDRS(symbol).getCur()
 
 if __name__ == "__main__":
     test = IMP()
